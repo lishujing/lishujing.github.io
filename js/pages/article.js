f4b444cbcc7900eaa55fc8d64bc3f85a348758e1
lishujing <2432595225@qq.com>
2021-06-13 14:24:16 +0800

    更新

--- js/pages/article.js
+++ js/pages/article.js
@@ -4,7 +4,7 @@
       //一段正则，匹配所有_min.的图片src属性
       var test = /_min\./
       //遍历所有的图片节点
-      $("#banner").css("background"," url('../../assets/images/banner3.jpg') center #000 no-repeat");
+      //$("#banner").css("background"," url('../../assets/images/banner3.jpg') center #000 no-repeat");
         		
       
     })
